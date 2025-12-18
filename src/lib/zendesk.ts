30e11e74848ec7f6a241b745d79aa8e68886faa3
Alix Henriot <alix@>
2025-12-18 20:43:09 +0100

    Nest Zendesk App Framework and update package.json for concurrent dev scripts

--- src/lib/zendesk.ts
+++ src/lib/zendesk.ts
@@ -2,6 +2,6 @@ import zendesk from "node-zendesk";
 
 export const zendeskClient = zendesk.createClient({
   username: process.env.ZENDESK_EMAIL,
-  password: process.env.ZENDESK_API_TOKEN,
+  token: process.env.ZENDESK_API_TOKEN,
   subdomain: process.env.ZENDESK_SUBDOMAIN,
 });

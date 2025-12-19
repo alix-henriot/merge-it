434e7a95ad72fb049a111453685be9755b877121
Alix Henriot <alix@>
2025-12-19 11:53:40 +0100

    Update background color values

--- src/components/ticket/status-badge.tsx
+++ src/components/ticket/status-badge.tsx
@@ -3,12 +3,12 @@ import { Badge } from "@/components/ui/badge";
 import { Ticket } from "node-zendesk/clients/core/tickets";
 
 const STATUS_COLOR_MAP: Record<string, string> = {
-  new: "bg-yellow-600",
-  open: "bg-red-600",
-  pending: "bg-blue-600",
+  new: "bg-yellow-800",
+  open: "bg-red-800",
+  pending: "bg-blue-800",
   hold: "bg-blue-900",
-  closed: "bg-gray-600",
-  solved: "bg-gray-600",
+  closed: "bg-gray-800",
+  solved: "bg-gray-800",
 };
 
 const BASE_STYLES = "text-white text-xs capitalize font-normal font-semibold";

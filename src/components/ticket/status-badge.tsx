9520ef7a1974c784f11242c5075ebc1f451f29c9
Alix Henriot <henriotal@icloud.com>
2026-01-26 22:31:09 +0100

    Refactor StatusBadge component to use cva for styling

--- src/components/ticket/status-badge.tsx
+++ src/components/ticket/status-badge.tsx
@@ -1,23 +1,27 @@
 import { cn } from "@/lib/cn";
+import { cva } from "class-variance-authority";
 import { Badge } from "@/components/ui/badge";
 import { Ticket } from "node-zendesk/clients/core/tickets";
 
-const STATUS_COLOR_MAP: Record<string, string> = {
-  new: "bg-yellow-800",
-  open: "bg-red-800",
-  pending: "bg-blue-800",
-  hold: "bg-blue-900",
-  closed: "bg-gray-800",
-  solved: "bg-gray-800",
-};
+const badgeStatus = cva(
+  "text-white capitalize font-normal",
+  {
+    variants: {
+      status: {
+        new: "bg-yellow-800",
+        open: "bg-red-800",
+        pending: "bg-blue-800",
+        hold: "bg-blue-900",
+        closed: "bg-gray-800",
+        solved: "bg-gray-800",
+      },
+    },
+  }
+);
 
-const BASE_STYLES = "text-white capitalize font-normal";
-
-export default function StatusBadge({ status }: Ticket) {
-  const bgColor = STATUS_COLOR_MAP[status || "closed"] || STATUS_COLOR_MAP.closed;
-
+export default function StatusBadge({ status }: Pick<Ticket, "status">) {
   return (
-    <Badge variant="outline" className={cn(bgColor, BASE_STYLES)}>
+    <Badge variant="outline" className={cn(badgeStatus({ status }))}>
       {status}
     </Badge>
   );

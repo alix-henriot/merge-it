de708af73797962128d9f1d3ae7db1f337aad275
Alix Henriot <henriotal@icloud.com>
2026-01-26 23:34:28 +0100

    - Add custom color variables - Remove unused getRequesterTickets component

--- src/lib/zendesk/tickets.ts
+++ src/lib/zendesk/tickets.ts
@@ -4,33 +4,7 @@ import { auth } from "@/lib/auth";
 import { Ticket } from "node-zendesk/clients/core/tickets";
 import { client } from "./client";
 
-export async function getRequesterTickets(id: number): Promise<Ticket[]> {
-  if (!id) {
-    throw new Error("User ID is required");
-  }
-
-  const session = await auth();
-
-  if (!session) {
-    throw new Error("Not authenticated with Zendesk");
-  }
-
-  const zendesk = client(session);
-
-  try {
-    //⚠️ Returned type for client.requests.listByUser(userId) has been manually overwritten from Promise<object[]> to Promise<Ticket[]> to remove type error. This should be fixed later on
-    return (await zendesk.requests.listByUser(id, { sort_order: "desc" })).slice(0, 4);
-  } catch (error) {
-    console.error("Zendesk requester tickets error:", error);
-    throw new Error("Failed to fetch requester tickets");
-  }
-}
-
-export async function createTicket(id: number): Promise<{ response: object; result: Ticket }> {
-  if (!id) {
-    throw new Error("User ID is required");
-  }
-
+export async function createTicket(): Promise<{ response: object; result: Ticket }> {
   const session = await auth();
 
   if (!session) {

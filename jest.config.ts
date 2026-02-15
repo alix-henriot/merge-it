939ee30af3599bbb2944591e0d046d2f15d380f6
Alix Henriot <henriotal@icloud.com>
2026-02-15 22:39:13 +0100

    Update Jest configuration: remove unused transform settings and add jest.setup.ts for testing utilities

--- jest.config.ts
+++ jest.config.ts
@@ -11,12 +11,7 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   testMatch: ["**/tests/**/*.+(ts|tsx|js)"],
-  transform: {
-    "^.+\\.(ts|tsx)$": ["ts-jest", { useESM: true }],
-  },
-  extensionsToTreatAsEsm: [".ts"],
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
